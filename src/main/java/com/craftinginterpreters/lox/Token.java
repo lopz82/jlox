a689dcaec0e5fbf99b2a239245c9e4abbc04fc6f
lopz82 <jorge.lopez.guzman@outlook.es>
2022-09-07 23:04:36 +0200

    Add equals and hashCode for comparison

--- src/main/java/com/craftinginterpreters/lox/Token.java
+++ src/main/java/com/craftinginterpreters/lox/Token.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 
+import java.util.Objects;
+
 public class Token {
     final TokenType type;
     final String lexeme;
@@ -15,5 +17,27 @@ public class Token {
 
     public String toString() {
         return type + " " + lexeme + " " + literal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Token token = (Token) o;
+
+        if (line != token.line) return false;
+        if (type != token.type) return false;
+        if (!Objects.equals(lexeme, token.lexeme)) return false;
+        return Objects.equals(literal, token.literal);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type != null ? type.hashCode() : 0;
+        result = 31 * result + (lexeme != null ? lexeme.hashCode() : 0);
+        result = 31 * result + (literal != null ? literal.hashCode() : 0);
+        result = 31 * result + line;
+        return result;
     }
 }
